151762c6a4f57767bc6e3a336c0056796989d406
Joan Andrés <andres6936@live.com>
2020-09-09 10:25:24 -0500

    Prepared class for inventory multiple selection

---
+++ Include/Cataclysm/Screen/InventoryMultipleSelectionScreen.hpp
@@ -0,0 +1,29 @@
+// Joan Andrés (@Andres6936) Github.
+
+#ifndef CATACLYSM_INVENTORYMULTIPLESELECTIONSCREEN_HPP
+#define CATACLYSM_INVENTORYMULTIPLESELECTIONSCREEN_HPP
+
+#include <Cataclysm/Screen/IScreen.hpp>
+#include <Cataclysm/Screen/InventoryMultipleSelectionScreen.hpp>
+
+namespace Cataclysm
+{
+
+	class InventoryMultipleSelectionScreen : public IScreen
+	{
+
+	public:
+
+		InventoryMultipleSelectionScreen();
+
+		void draw() override;
+
+		void updated() override;
+
+		ScreenType processInput() override;
+
+	};
+
+}
+
+#endif //CATACLYSM_INVENTORYMULTIPLESELECTIONSCREEN_HPP
---
+++ Source/Screen/InventoryMultipleSelectionScreen.cpp
@@ -0,0 +1,25 @@
+// Joan Andrés (@Andres6936) Github.
+
+#include "Cataclysm/Screen/InventoryMultipleSelectionScreen.hpp"
+
+using namespace Cataclysm;
+
+InventoryMultipleSelectionScreen::InventoryMultipleSelectionScreen()
+{
+
+}
+
+void InventoryMultipleSelectionScreen::draw()
+{
+
+}
+
+void InventoryMultipleSelectionScreen::updated()
+{
+
+}
+
+ScreenType InventoryMultipleSelectionScreen::processInput()
+{
+	return ScreenType::PLAY;
+}
