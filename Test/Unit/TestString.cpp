d2c218bbf677adbe4b769cdd5328279fc29fe351
Joan Andrés <andres6936@live.com>
2020-09-09 23:50:51 -0500

    Added new test case

--- Test/Unit/TestString.cpp
+++ Test/Unit/TestString.cpp
@@ -1,2 +1,9 @@
 // Joan Andrés (@Andres6936) Github.
 
+#include <doctest/doctest.h>
+#include <Cataclysm/Util/String/stringfunc.h>
+
+TEST_CASE("Remove tags color to string")
+{
+	CHECK(remove_color_tags("<c=green>Black<c=/>") == "Black");
+}
